9c4c14c0e520b4fbd1627e74e4ef66b6bff5c2b5
artibarti <safranyartur1995@gmail.com>
2018-10-20 15:34:53 +0200

    Initial commit

---
+++ src/main/java/com/buildtwicebulldozeonce/universiteanonyme/UniversiteAnonymeApplication.java
@@ -0,0 +1,12 @@
+package com.buildtwicebulldozeonce.universiteanonyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversiteAnonymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversiteAnonymeApplication.class, args);
+	}
+}
