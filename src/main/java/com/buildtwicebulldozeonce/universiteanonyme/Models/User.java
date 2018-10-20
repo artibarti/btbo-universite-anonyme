163fc9e830ee754dd72b31b92f2a72255109728f
Kummer156 <wmarci15@gmail.com>
2018-10-20 17:52:20 +0200

    Annotate the user class to become an entity

--- src/main/java/com/buildtwicebulldozeonce/universiteanonyme/Models/User.java
+++ src/main/java/com/buildtwicebulldozeonce/universiteanonyme/Models/User.java
@@ -1,18 +1,29 @@
 package com.buildtwicebulldozeonce.universiteanonyme.Models;
 
 import lombok.Data;
+import lombok.Generated;
 import lombok.NoArgsConstructor;
 import lombok.extern.java.Log;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 @Data
 @NoArgsConstructor
 @Log
 public class User {
-
+    @Id
+    @Generated
     private int id;
+    @Column
     private String email;
+    @Column
     private String doubleHashedPassword;
+    @Column
     private int anonym_id;
+    @Column
     private String name;
     // picture
     // permission
