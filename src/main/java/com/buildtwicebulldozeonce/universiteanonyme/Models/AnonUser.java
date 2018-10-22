8d1a92c8396104a885fa1479070f38a64fd12b8a
Auror <czinegez@gmail.com>
2018-10-22 20:39:59 +0200

    User changes New Permission class

--- src/main/java/com/buildtwicebulldozeonce/universiteanonyme/Models/AnonUser.java
+++ src/main/java/com/buildtwicebulldozeonce/universiteanonyme/Models/AnonUser.java
@@ -4,12 +4,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.java.Log;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 @Data
 @NoArgsConstructor
 @Log
 public class AnonUser {
+    @Id
     private int id;
+    @Column(nullable = false, unique = true)
     private String anonName;
+    @Column(nullable = false)
     private String hashedPassword;
     //rating
     //picture
