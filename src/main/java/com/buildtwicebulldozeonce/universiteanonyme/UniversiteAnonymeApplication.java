03a124a166f4ac4eaae5aa224a4ed284b372947a
Auror <czinegez@gmail.com>
2018-10-22 23:43:40 +0200

    User changes New Permission class

--- src/main/java/com/buildtwicebulldozeonce/universiteanonyme/UniversiteAnonymeApplication.java
+++ src/main/java/com/buildtwicebulldozeonce/universiteanonyme/UniversiteAnonymeApplication.java
@@ -1,28 +1,11 @@
 package com.buildtwicebulldozeonce.universiteanonyme;
 
-import com.buildtwicebulldozeonce.universiteanonyme.Models.Course;
-import com.buildtwicebulldozeonce.universiteanonyme.Models.User;
-import com.buildtwicebulldozeonce.universiteanonyme.Services.CourseService;
-import com.buildtwicebulldozeonce.universiteanonyme.Services.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UniversiteAnonymeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UniversiteAnonymeApplication.class, args);
-
-		User user = new User();
-		user.setName("TestUser0");
-
-        Course course = new Course();
-        course.setOwner(user);
-        course.setName("TestCourse0");
-
-        UserService userService = new UserService();
-        userService.addUser(user);
-        CourseService courseService = new CourseService();
-        courseService.addCourse(course);
 	}
 }
