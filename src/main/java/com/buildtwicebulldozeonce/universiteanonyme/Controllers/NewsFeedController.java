5d724503eccb2635f51e7230a62dadd2426fc8bd
artibarti <safranyartur1995@gmail.com>
2018-12-03 03:45:15 +0100

    token

--- src/main/java/com/buildtwicebulldozeonce/universiteanonyme/Controllers/NewsFeedController.java
+++ src/main/java/com/buildtwicebulldozeonce/universiteanonyme/Controllers/NewsFeedController.java
@@ -1,9 +1,11 @@
 package com.buildtwicebulldozeonce.universiteanonyme.Controllers;
 
+import com.buildtwicebulldozeonce.universiteanonyme.Helpers.Functions;
 import com.buildtwicebulldozeonce.universiteanonyme.Models.News;
 import com.buildtwicebulldozeonce.universiteanonyme.Services.NewsFeedService;
 import com.buildtwicebulldozeonce.universiteanonyme.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,11 +25,14 @@ public class NewsFeedController
         this.userService = userService;
     }
 
-    @RequestMapping(value = "/users/{id}/newsfeed", method = RequestMethod.GET)
-    public List<News> getNewFeedForUser(@PathVariable("id") int id)
+    @RequestMapping(value = "/user/newsfeed", method = RequestMethod.GET)
+    public List<News> getNewFeedForUser(@RequestHeader HttpHeaders headers)
     {
-        return this.newsFeedService.getNewsFeedForUser(id,
-                this.userService.getLoggedInUserByUserId(id).getValue2().getId());
+        String token = Functions.getValueFromHttpHeader(headers,"token");
+
+        int anonID = this.userService.getLoggedInUser(token).getValue2().getId();
+        int id = this.userService.getLoggedInUser(token).getValue1().getId();
+        return this.newsFeedService.getNewsFeedForUser(id, anonID);
     }
 
 }
